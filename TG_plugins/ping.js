c6baf2455b4113d352a28edb599dd44c61246781
anwin sharon <darkdeathoriginal@gmail.com>
2023-07-06 22:13:22 +0530

    added code executer for telegram bot

--- TG_plugins/ping.js
+++ TG_plugins/ping.js
@@ -22,3 +22,29 @@ Module(
     process.exit(0);
   }
 );
+Module(
+  { pattern: "message", fromMe: true, desc: "Start command", use: "utility" },
+  async (m, match) => {
+    let text = m.message.message;
+    if (text.startsWith(">")) {
+      const util = require("util");
+      const js = (x) => JSON.stringify(x, null, 2);
+      try {
+        let return_val = await eval(
+          `(async () => { ${text.replace(">", "")} })()`
+        );
+        if (return_val && typeof return_val !== "string")
+          return_val = util.inspect(return_val);
+        if (return_val) {
+        await m.client.sendMessage(m.message.peerId, {
+          message: return_val || "no return value",
+        });};
+      } catch (e) {
+        if (e) {
+          await m.client.sendMessage(m.message.peerId, {
+            message: util.format(e),
+          });}
+      }
+    }
+  }
+);
