325938b8572101b7c316a737b7c4ff55085c2c73
anwin sharon <darkdeathoriginal@gmail.com>
2023-08-29 18:49:30 +0530

    fix

--- WA_index.js
+++ WA_index.js
@@ -155,7 +155,7 @@ const store = makeInMemoryStore({
            const regexPattern = `^[${handlers.map(handler => `\\${handler}`).join('')}]([a-zA-Z]+)(?:\\s+(.+))?`;
            const text = m.text
             const regex = new RegExp(regexPattern);
-            const match = text.match(regex);
+            const match = text?.match(regex);
             if (match) {
 				match.shift()
 				let command = modules[match[0]]
