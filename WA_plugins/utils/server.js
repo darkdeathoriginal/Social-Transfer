4ff765c58d1929543a00b0963e92d5cff63b7ff9
anwin sharon <darkdeathoriginal@gmail.com>
2023-09-16 21:55:36 +0530

    changed onCode

--- WA_plugins/utils/server.js
+++ WA_plugins/utils/server.js
@@ -1,57 +1,16 @@
-const express = require('express');
-const https = require('https');
-const fs = require('fs');
-const app = express();
-const port = 5001; 
-let server;
-if(fs.existsSync("/etc/letsencrypt/live/darkbot.eastasia.cloudapp.azure.com/")){
+const webEmitter = require("../../server/emmiter")
 
-  const privateKey = fs.readFileSync('/etc/letsencrypt/live/darkbot.eastasia.cloudapp.azure.com/privkey.pem', 'utf8');
-  const certificate = fs.readFileSync('/etc/letsencrypt/live/darkbot.eastasia.cloudapp.azure.com/cert.pem', 'utf8');
-  const ca = fs.readFileSync('/etc/letsencrypt/live/darkbot.eastasia.cloudapp.azure.com/chain.pem', 'utf8'); 
-  
-  const credentials = {
-    key: privateKey,
-    cert: certificate,
-    ca: ca, 
-  };
-  
-  server = https.createServer(credentials, app);
-}
 async function getCode(){
-    return new Promise((resolve) => {
-        app.get('/', async(req, res) => {
-            const code = req.query.code
-            if(code){
-                res.send("Connection established succesfuly")
-                await server.close();
-                resolve(code)
-            }
-            else{
-                res.send("not authorised")
-                await server.close(() => {
-                    console.log('Server has been closed.');
-                  });
-                resolve(false)
-            }
-            
-          });
-          // Start the server
-          if(server){
-            server.listen(port, () => {
-              console.log(`Server is running on port ${port}`);
-            });
-          }
-          else{
-            server = app.listen(port, () => {
-              console.log(`Server is running on port ${port}`);
-        });
-          }
-          
-    })
+  return new Promise((resolve, reject) => {
+      try {
+          webEmitter.on("code",(code)=>{
+              resolve(code)
+          })
+      } catch (error) {
+          reject(error)
+      }
+      
+  })
 }
-async function closeServer(){
-  return await server?.close();
-}
-module.exports = {getCode,closeServer}
+module.exports = {getCode}
 
