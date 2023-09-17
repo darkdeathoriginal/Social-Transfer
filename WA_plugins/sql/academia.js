144e48f0108d4eb72b16411dc726d58b7de42847
anwin sharon <darkdeathoriginal@gmail.com>
2023-09-17 12:30:44 +0530

    added moal for academia

---
+++ WA_plugins/sql/academia.js
@@ -0,0 +1,23 @@
+const config = require('../../config');
+const { DataTypes } = require('sequelize');
+
+const academiaDb = config.DATABASE.define('academia', {
+    netid: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    token: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    data:{
+        type:DataTypes.JSON,
+        allowNull:false
+    },
+    jid:{
+        type:DataTypes.STRING,
+        allowNull:false
+    }
+});
+
+module.exports = academiaDb
