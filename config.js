49c953d383dcb3170ccb896128d0e5a1ac8226e3
Anwin Sharon <87226858+darkdeathoriginal@users.noreply.github.com>
2024-03-04 18:34:11 +0530

    fixed env and added handler env

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 DATABASE_URL = process.env.DATABASE_URL === undefined ? './bot.db' : process.env.DATABASE_URL;
 const { Sequelize } = require('sequelize');
 
@@ -6,7 +7,7 @@ module.exports ={
     DATABASE_URL: DATABASE_URL,
     DATABASE: DATABASE_URL === './bot.db' ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: false }) : new Sequelize(DATABASE_URL, { dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }, logging: false }),   
     SUDO:['919072215994@s.whatsapp.net','14404448898:22@s.whatsapp.net',''],
-    HANDLERS:["."],
+    HANDLERS:process.env.HANDLERS?.split(" ")??["."],
     SERVER:process.env.SERVER || "https://darkbot.centralindia.cloudapp.azure.com",
     WASESSION:process.env.WASESSION,
     PORT:process.env.PORT || 3000,
