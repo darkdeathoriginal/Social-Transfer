5a469b06d9be85239bba9d808f5799c2b074f09b
Anwin Sharon <87226858+darkdeathoriginal@users.noreply.github.com>
2024-03-20 15:12:27 +0530

    fix

--- server/Routes/code.js
+++ server/Routes/code.js
@@ -1,18 +1,19 @@
-const express = require("express")
-const webEmitter = require("../emmiter")
-const router = express.Router()
+const express = require("express");
+const webEmitter = require("../emmiter");
+const router = express.Router();
 
 
-router.get("/",(req,res)=>{
-    const code = req.query.code
-    if(code){
-        webEmitter.emit("code",code)
-        res.send("Connection established succesfuly")
-    }
-    else{
-        res.send("not authorised")
-    }
-    
-})
+router.get("/code", (req, res) => {
+  const code = req.query.code;
+  if (code) {
+    webEmitter.emit("code", code);
+    res.send("Connection established succesfuly");
+  } else {
+    res.send("not authorised");
+  }
+});
+router.get("/", (req, res) => {
+    res.send("not authorised");
+  });
 
-module.exports = router
+module.exports = router;
