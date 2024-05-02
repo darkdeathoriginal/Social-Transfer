e786d99a8142bb2de89b43eca8e7bfed58d5fe20
Anwin Sharon <87226858+darkdeathoriginal@users.noreply.github.com>
2024-05-02 22:35:59 +0530

    added plugin update command

--- WA_plugins/external_plugins.js
+++ WA_plugins/external_plugins.js
@@ -1,105 +1,176 @@
-const {
-    Module
-} = require('../WA_index');
-const Config = require('../config');
-const axios = require('axios');
-const fs = require('fs');
-const Db = require('./sql/plugin');
+const { Module } = require("../WA_index");
+const Config = require("../config");
+const axios = require("axios");
+const fs = require("fs");
+const Db = require("./sql/plugin");
+const { pattern } = require("pdfkit");
 
-
-Module({
-    pattern: 'inp',
+Module(
+  {
+    pattern: "inp",
     fromMe: true,
-    use: 'owner',
-    desc: "plugin installer"
-}, (async (message, match) => {
-    match = match[1]
-    if (!match || !/\bhttps?:\/\/\S+/gi.test(match)) return await message.send("need url")
+    use: "owner",
+    desc: "plugin installer",
+  },
+  async (message, match) => {
+    match = match[1];
+    if (!match || !/\bhttps?:\/\/\S+/gi.test(match))
+      return await message.send("need url");
     await Db.PluginDB.sync();
     let links = match.match(/\bhttps?:\/\/\S+/gi);
-    for (let link of links){
-    try {
+    for (let link of links) {
+      try {
         var url = new URL(link);
-    } catch {
+      } catch {
         return await message.send("invalid url");
-    }
-    if (url.host === 'gist.github.com' || url.host === 'gist.githubusercontent.com') {
-        url = !url?.toString().endsWith('raw')?url.toString() + '/raw':url.toString()
-    } else {
-        url = url.toString()
-    }
-    try {
-        var response = await axios(url+"?timestamp="+new Date());
-    } catch {
-        return await message.send("invalid url")
-    }
-    let plugin_name = /pattern: ["'](.*)["'],/g.exec(response.data)
-    var plugin_name_temp = response.data.match(/pattern: ["'](.*)["'],/g)?response.data.match(/pattern: ["'](.*)["'],/g).map(e=>e.replace("pattern","").replace(/[^a-zA-Z]/g, "")):"temp"
-    try { plugin_name = plugin_name[1].split(" ")[0] } catch { return await message.sendReply("_Invalid plugin. No plugin name found!_") }
-    fs.writeFileSync('./WA_plugins/' + plugin_name + '.js', response.data);
-    plugin_name_temp = plugin_name_temp.length > 1 ? plugin_name_temp.join(", ") : plugin_name;
-    try {
-        require('./' + plugin_name);
-    } catch (e) {
-        fs.unlinkSync(__dirname+'/'+plugin_name + '.js')
+      }
+      if (
+        url.host === "gist.github.com" ||
+        url.host === "gist.githubusercontent.com"
+      ) {
+        url = !url?.toString().endsWith("raw")
+          ? url.toString() + "/raw"
+          : url.toString();
+      } else {
+        url = url.toString();
+      }
+      try {
+        var response = await axios(url + "?timestamp=" + new Date());
+      } catch {
+        return await message.send("invalid url");
+      }
+      let plugin_name = /pattern: ["'](.*)["'],/g.exec(response.data);
+      var plugin_name_temp = response.data.match(/pattern: ["'](.*)["'],/g)
+        ? response.data
+            .match(/pattern: ["'](.*)["'],/g)
+            .map((e) => e.replace("pattern", "").replace(/[^a-zA-Z]/g, ""))
+        : "temp";
+      try {
+        plugin_name = plugin_name[1].split(" ")[0];
+      } catch {
+        return await message.sendReply(
+          "_Invalid plugin. No plugin name found!_"
+        );
+      }
+      fs.writeFileSync("./WA_plugins/" + plugin_name + ".js", response.data);
+      plugin_name_temp =
+        plugin_name_temp.length > 1 ? plugin_name_temp.join(", ") : plugin_name;
+      try {
+        require("./" + plugin_name);
+      } catch (e) {
+        fs.unlinkSync(__dirname + "/" + plugin_name + ".js");
         return await message.send("invalid plugin\n" + e);
+      }
+      await Db.installPlugin(url, plugin_name);
+      await message.send(plugin_name_temp + " installed.");
     }
-    await Db.installPlugin(url, plugin_name);
-    await message.send(plugin_name_temp+" installed.");
-}
-}));
+  }
+);
 
-Module({
-    pattern: 'plugin',
+Module(
+  {
+    pattern: "plugin",
     fromMe: true,
-    use: 'owner',
-    desc: "plugin list"
-}, (async (message, match) => {
+    use: "owner",
+    desc: "plugin list",
+  },
+  async (message, match) => {
     await Db.PluginDB.sync();
     var plugins = await Db.PluginDB.findAll();
-    if (match[1]&&match[1] !== '') {
-        var plugin = plugins.filter(_plugin => _plugin.dataValues.name === match[1])
-        try {
-            await message.send(`_${plugin[0].dataValues.name}:_ ${plugin[0].dataValues.url}`);
-        } catch {
-            return await message.send("plugin not found")
-        }
-        return;
+    if (match[1] && match[1] !== "") {
+      var plugin = plugins.filter(
+        (_plugin) => _plugin.dataValues.name === match[1]
+      );
+      try {
+        await message.send(
+          `_${plugin[0].dataValues.name}:_ ${plugin[0].dataValues.url}`
+        );
+      } catch {
+        return await message.send("plugin not found");
+      }
+      return;
     }
     var msg = "\n";
     if (plugins.length < 1) {
-        console.log(plugins);
-        return await message.send("plugin not found");
+      console.log(plugins);
+      return await message.send("plugin not found");
     } else {
-        plugins.map(
-            (plugin) => {
-                msg += '*' +plugin.dataValues.name + '* : ' +( plugin.dataValues.url.endsWith("/raw")?plugin.dataValues.url.replace('raw',''):plugin.dataValues.url) + '\n\n';
-            }
-        );
-        return await message.send(msg);
+      plugins.map((plugin) => {
+        msg +=
+          "*" +
+          plugin.dataValues.name +
+          "* : " +
+          (plugin.dataValues.url.endsWith("/raw")
+            ? plugin.dataValues.url.replace("raw", "")
+            : plugin.dataValues.url) +
+          "\n\n";
+      });
+      return await message.send(msg);
     }
-}));
+  }
+);
 
-Module({
-    pattern: 'rmp',
+Module(
+  {
+    pattern: "rmp",
     fromMe: true,
-    use: 'owner',
-    desc: "plugin remover"
-}, (async (message, match) => {
-    if (match[1] === '') return await message.send("need plugin");
+    use: "owner",
+    desc: "plugin remover",
+  },
+  async (message, match) => {
+    if (match[1] === "") return await message.send("need plugin");
     await Db.PluginDB.sync();
     var plugin = await Db.PluginDB.findAll({
-        where: {
-            name: match[1]
-        }
+      where: {
+        name: match[1],
+      },
     });
     if (plugin.length < 1) {
-        return await message.send("plugin not found");
+      return await message.send("plugin not found");
     } else {
-        await plugin[0].destroy();
-        const Message = match[1]+" removed succesfully" 
-        await message.send(Message);
-        delete require.cache[require.resolve('./' + match[1] + '.js')]
-        fs.unlinkSync('./WA_plugins/' + match[1] + '.js');
-       }
-}));
+      await plugin[0].destroy();
+      const Message = match[1] + " removed succesfully";
+      await message.send(Message);
+      delete require.cache[require.resolve("./" + match[1] + ".js")];
+      fs.unlinkSync("./WA_plugins/" + match[1] + ".js");
+    }
+  }
+);
+
+Module(
+  {
+    pattern: "udp",
+    fromMe: true,
+    use: "owner",
+    desc: "update plugin",
+  },
+  async (m, match) => {
+    const plugin = match[1];
+    if (!plugin) return await m.send("need plugin");
+    await Db.PluginDB.sync();
+    var plugins = await Db.PluginDB.findAll({
+      where: {
+        name: plugin,
+      },
+    });
+    if (plugins.length < 1) {
+      return await m.send("plugin not found");
+    }
+    var url = plugins[0].dataValues.url;
+    try {
+      var response = await axios(url + "?timestamp=" + new Date());
+    } catch {
+      return await m.send("invalid url");
+    }
+    fs.writeFileSync("./WA_plugins/" + plugin + ".js", response.data);
+    delete require.cache[require.resolve("./" + plugin + ".js")];
+    try {
+      require("./" + plugin);
+    } catch (e) {
+      fs.unlinkSync(__dirname + "/" + plugin + ".js");
+      return await m.send("invalid plugin\n" + e);
+    }
+    await m.send(plugin + " updated.");
+    return;
+  }
+);
